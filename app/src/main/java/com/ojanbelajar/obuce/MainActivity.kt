edc99ba78d41325d11bc88257067fea7def6e1e8
KillerQueen59 <ramadhan_tiba@apps.ipb.ac.id>
2021-09-01 16:59:36 +0700

    first commit

---
+++ app/src/main/java/com/ojanbelajar/obuce/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ojanbelajar.obuce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
