640f7f80f0824278f96544c39da9f33a22360d00
Anggri <hendrika_ang@apps.ipb.ac.id>
2021-09-26 22:23:45 +0700

    usecase 4
    add camera feature

--- app/src/main/java/com/ojanbelajar/obuce/ui/tab/home/food/scan/HowToScanActivity.kt
+++ app/src/main/java/com/ojanbelajar/obuce/ui/tab/home/food/scan/HowToScanActivity.kt
@@ -1,7 +1,11 @@
 package com.ojanbelajar.obuce.ui.tab.home.food.scan
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.ojanbelajar.obuce.R
 import com.ojanbelajar.obuce.databinding.ActivityHowToScanBinding
 import org.jetbrains.anko.startActivity
@@ -16,7 +20,26 @@ class HowToScanActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.scanFood.setOnClickListener{
+            dispatchTakePictureIntent()
+            //startActivity<ScanResultActivity>()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //val imageBitmap = data.extras.get("data") as Bitmap
+            //imageView.setImageBitmap(imageBitmap)
             startActivity<ScanResultActivity>()
+        }
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
     }
 }
